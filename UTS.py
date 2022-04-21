7793baa2c613ad4ea1788f2e473855f9db222977
Rendykiki <88619286+Rendykiki@users.noreply.github.com>
2022-04-21 21:46:40 +0700

    Add files via upload

---
+++ UTS.py
@@ -0,0 +1,16 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+  fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
